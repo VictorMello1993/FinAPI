9d59a56bb42aaf60c4836da4182a135bae333ae0
Victor Mello <victorsmello93@gmail.com>
2021-09-19 00:27:35 -0300

    Implementados os endpoints de depósito, saque, listagem de extratos por data, atualização e remoção do cadastro, e de obter o saldo da conta

--- README.md
+++ README.md
@@ -5,12 +5,13 @@ Primeiro projeto a ser desenvolvido durante o módulo 1 da trilha Node.js do Ign
 
 - [x] Deve ser possível criar umma conta
 - [x] Deve ser possível buscar o extrato bancário do cliente
-- [] Deve ser possível realizar um depósito
-- [] Deve ser possível realizar um saque
-- [] Deve ser possível buscar o extrato bancário do cliente por data
-- [] Deve ser possível atualizar dados da conta do cliente
-- [] Deve ser possível obter dados da conta do cliente
-- [] Deve ser possível deletar uma conta
+- [x] Deve ser possível realizar um depósito
+- [x] Deve ser possível realizar um saque
+- [x] Deve ser possível buscar o extrato bancário do cliente por data
+- [x] Deve ser possível atualizar dados da conta do cliente
+- [x] Deve ser possível obter dados da conta do cliente
+- [x] Deve ser possível deletar uma conta
+- [x] Deve ser possível retornar o balanço 
 
 ---
 
@@ -18,17 +19,11 @@ Primeiro projeto a ser desenvolvido durante o módulo 1 da trilha Node.js do Ign
 
 - [x] Não deve ser possível cadastrar uma conta com CPF já existente
 - [x] Não deve ser possível fazer depósito em uma conta não existente
-- [] Não deve ser possível buscar extrato em uma conta não existente
-- [] Não deve ser possível fazer saque em uma conta não existente
-- [] Não deve ser possível excluir uma conta não existente
-- [] Não deve ser possível fazer saque quando o saldo for insuficiente
+- [x] Não deve ser possível buscar extrato em uma conta não existente
+- [x] Não deve ser possível fazer saque em uma conta não existente
+- [x] Não deve ser possível excluir uma conta não existente
+- [x] Não deve ser possível fazer saque quando o saldo for insuficiente
 
 ## Atributos
 Ao criar uma conta, o cliente irá receber o nome, CPF e o extrato conténdo os lançamentos de crédito e débito
-
-- [ ] Não deve ser possível cadastrar uma conta com CPF já existente
-- [ ] Não deve ser possível fazer depósito em uma conta não existente
-- [ ] Não deve ser possível buscar extrato em uma conta não existente
-- [ ] Não deve ser possível fazer saque em uma conta não existente
-- [ ] Não deve ser possível excluir uma conta não existente
-- [ ] Não deve ser possível fazer saque quando o saldo for insuficiente
+Um extrato contém a descrição, o saldo e o tipo de lançamento (crédito ou débito)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express')
 const express = require('express')
 const {v4: uuidv4} = require('uuid')
 
@@ -7,8 +8,38 @@ app.use(express.json())
 
 const customers = []
 
+// Middleware
+function verifyIfExistsAccountByCPF(req, res, next){
+  const {cpf} = req.headers
+
+  //Buscando o cliente existente
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if(!customer){
+    return res.status(404).json({error: "Customer not found"})
+  }
+  
+  req.customer = customer
+
+  return next()
+}
+
+function getBalance(statements){
+  // let sum = 0
+  const balance = statements.reduce((acc, statement) => {
+    if(statement.type === 'credit'){
+      return acc + statement.amount
+    }
+    else{
+      return acc - statement.amount
+    }
+  }, 0)
+
+  return balance
+}
+
 //1 - Criando uma conta do cliente      
-app.use("/accounts", (req, res) => {
+app.post("/accounts", (req, res) => {
   
   const {cpf, name} = req.body
 
@@ -24,18 +55,102 @@ app.use("/accounts", (req, res) => {
   return res.status(201).send()
 })
 
-//2 - Buscando o extrato bancário do cliente
-app.get('/statements', (req, res) => {
-  const {cpf} = req.headers
+//2 - Buscando todos os extratos bancários do cliente
+app.get('/statements', verifyIfExistsAccountByCPF, (req, res) => {
+  const {customer} = req
+  return res.json(customer.statement)
+})
 
-  //Buscando o cliente existente
-  const customer = customers.find(customer => customer.cpf === cpf)
+//Todas as rotas deverão passar pelo middleware abaixo antes de completar a requisição
+// app.use(verifyIfExistsAccountByCPF)
 
-  if(!customer){
-    return res.status(404).json({error: "Customer not found"})
+// 3 - Realizando depósito
+app.post("/deposit", verifyIfExistsAccountByCPF, (req, res) => {
+  const {description, amount} = req.body
+  const {customer} = req
+
+  const statementDto = {
+    description,
+    amount,
+    type: 'credit',
+    created_at: new Date().toLocaleDateString()
   }
 
-  return res.json(customer.statement)
+  customer.statement.push(statementDto)
+  
+  return res.status(201).send()
+})
+
+// 4 - Realizando saque
+app.post("/withdraw", verifyIfExistsAccountByCPF, (req, res) => {
+  const {amount} = req.body
+  const {customer} = req
+
+  const balance = getBalance(customer.statement)
+
+  if(balance < amount){
+    return res.status(400).json({message: "Not enough funds!"})
+  }  
+  
+  const statementDto = {    
+    amount,
+    type: 'debit',
+    created_at: new Date().toLocaleDateString()
+  }
+
+  customer.statement.push(statementDto)
+
+  return res.status(201).send()
+})
+
+// 5 - Obtendo extratos bancários do cliente por data
+app.get('/statements/date', verifyIfExistsAccountByCPF, (req, res) => {
+  const {customer} = req
+  const {date} = req.query
+
+  const statementByDate = customer.statement.filter(statement => statement.created_at === date)
+
+  if(!statementByDate){
+    return res.status(404).json({message: 'Statement not found'})
+  }
+
+  return res.json(statementByDate)
+})
+
+//6 - Atualizando os dados do cliente
+app.put('/accounts', verifyIfExistsAccountByCPF, (req, res) => {
+  const {name} = req.body
+  const {customer} = req
+  
+  customer.name = name
+
+  return res.status(204).send()
+})
+
+//7 - Listando os dados da conta do cliente 
+app.get('/accounts', verifyIfExistsAccountByCPF, (req, res) => {  
+  const {customer} = req
+
+  return res.json(customer)
+})
+
+//8 - Excluindo uma conta 
+app.delete('/accounts', verifyIfExistsAccountByCPF, (req, res) => { 
+  const {customer} = req
+
+  customers.pop(customer)
+
+  return res.status(204).json(customers)
+
+})
+
+//9 - Obtendo o saldo da conta do cliente 
+app.get('/balance', verifyIfExistsAccountByCPF, (req, res) => { 
+  const {customer} = req
+
+  const balance = getBalance(customer.statement)
+
+  return res.json(balance)
 })
 
 app.listen(3000, () => console.log('Server is running...'))
